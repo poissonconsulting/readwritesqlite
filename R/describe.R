261f56a4e3233671f64fda941f220408de9957e8
joethorley <joe@poissonconsulting.ca>
2019-07-03 11:26:49 -0700

    fix rws_describe_meta replacing absent with NA

--- R/describe.R
+++ R/describe.R
@@ -5,7 +5,7 @@
 #' @return An invisible copy of the updated meta table.
 #' @family rws_describe_meta
 #' @export
-rws_describe_meta <- function(x, ..., strict = TRUE, conn) {
+rws_describe_meta <- function(x, ..., conn) {
   UseMethod("rws_describe_meta")
 }
 
@@ -15,9 +15,6 @@ rws_describe_meta <- function(x, ..., strict = TRUE, conn) {
 #' @param x A character vector of table name(s).
 #' @param column A character vector of column name(s).
 #' @param description A character vector of the description(s)
-#' @param paste0 A flag specifying whether to replace existing descriptions (FALSE)
-#'  or append using paste0 to existing descriptions (TRUE).
-#' @param strict A flag specifying whether to error if tables and/or columns do not exist (or ignore).
 #' @inheritParams rws_write
 #' 
 #' @return An invisible copy of the updated meta table.
@@ -32,34 +29,27 @@ rws_describe_meta <- function(x, ..., strict = TRUE, conn) {
 #' rws_describe_meta("rws_data", "POSIXct", "Time of the visit", conn =conn)
 #' rws_read_meta(conn)
 #' rws_disconnect(conn)
-rws_describe_meta.character <- function(x, column, description, ..., paste0 = FALSE, strict = TRUE, conn) {
+rws_describe_meta.character <- function(x, column, description, ..., conn) {
   check_vector(x, "")
   check_vector(column, "")
   check_vector(description, c("", NA))
-  check_flag(paste0)
-  check_flag(strict)
   check_sqlite_connection(conn, connected = TRUE)
   check_unused(...)
   
   rws_describe_meta(data.frame(
     Table = x, Column = column, Description = description, 
-    stringsAsFactors = FALSE), paste0 = paste0, strict = strict, conn = conn)
+    stringsAsFactors = FALSE), conn = conn)
 }
 
 #' Add Data Frame of Descriptions to SQL Meta Data Table
 #'
 #' @inheritParams rws_write
 #' @param x A data frame with columns Table, Column, Description.
-#' @param strict A flag specifying whether to error if tables and/or columns do not exist.
-#' @param paste0 A flag specifying whether to replace existing descriptions (FALSE)
-#'  or append using paste0 to existing descriptions (TRUE).
 #' @return An invisible character vector of the previous descriptions.
 #' @family rws_read
 #' @export
-rws_describe_meta.data.frame <- function(x, ..., paste0 = FALSE, strict = TRUE, conn) {
+rws_describe_meta.data.frame <- function(x, ..., conn) {
   check_data(x, values = list(Table = "", Column = "", Description = c("", NA)))
-  check_flag(paste0)
-  check_flag(strict)
   check_sqlite_connection(conn, connected = TRUE)
   check_unused(...)
   
@@ -76,22 +66,14 @@ rws_describe_meta.data.frame <- function(x, ..., paste0 = FALSE, strict = TRUE, 
   
   meta <- rws_read_meta(conn)
   meta$RowMeta <- 1:nrow(meta)
-  
+
   meta <- merge(meta, x, by.x = c("TableMeta", "ColumnMeta"), 
                 by.y = c("Table", "Column"), all = TRUE)
   
-  if(strict && any(is.na(meta$RowMeta))) 
+  if(any(is.na(meta$RowMeta))) 
     err("all description tables and columns must exist in the meta table")
   
-  meta <- meta[!is.na(meta$RowMeta),]
-  if(!paste0) {
-    meta$DescriptionMeta <- meta$Description
-  } else{
-    meta$DescriptionMeta[is.na(meta$DescriptionMeta)] <- meta$Description[is.na(meta$DescriptionMeta)]
-    bol <- !is.na(meta$DescriptionMeta) && !is.na(meta$Description)
-    meta$DescriptionMeta[bol] <- 
-      paste0(meta$DescriptionMeta[bol], meta$Description[bol])
-  }
+  meta$DescriptionMeta[!is.na(meta$Row)] <- meta$Description[!is.na(meta$Row)]
   meta <- meta[order(meta$RowMeta),]
   
   meta <- meta[c("TableMeta", "ColumnMeta", "MetaMeta", "DescriptionMeta")]
--- tests/testthat/test-describe.R
+++ tests/testthat/test-describe.R
@@ -30,7 +30,7 @@ test_that("describe with vector works", {
   expect_identical(rws_read_meta(conn), meta)
 })
 
-test_that("describe errors with strict", {
+test_that("describe errors if extra", {
   conn <- rws_connect(":memory:")
   teardown(rws_disconnect(conn))
   
@@ -38,14 +38,6 @@ test_that("describe errors with strict", {
   DBI::dbCreateTable(conn, "local", local)
   expect_identical(rws_write(local, conn = conn), "local")
   expect_error(rws_describe_meta("local", c("y", "Z2"), c("stuff", "A logical vector."), conn = conn), "all description tables and columns must exist in the meta table")
-  meta <- rws_describe_meta("local", c("y", "Z2"), c("stuff", "A logical vector."),
-                            strict = FALSE, conn = conn)
-  
-  expect_identical(meta, tibble::tibble(TableMeta = c("LOCAL", "LOCAL"),
-                                        ColumnMeta = c("Y", "Z"),
-                                        MetaMeta = c(NA, "class: logical"),
-                                        DescriptionMeta = c("stuff", NA)))
-  expect_identical(rws_read_meta(conn), meta)
 })
 
 test_that("describe replace works", {
@@ -65,20 +57,35 @@ test_that("describe replace works", {
                                         ColumnMeta = "Z",
                                         MetaMeta = "class: logical",
                                         DescriptionMeta = " More info."))
-  meta <- rws_describe_meta("local", "Z", " More info.", paste0 = TRUE, conn = conn)
-  expect_identical(meta, tibble::tibble(TableMeta = "LOCAL",
-                                        ColumnMeta = "Z",
-                                        MetaMeta = "class: logical",
-                                        DescriptionMeta = " More info. More info."))
   
-  meta <- rws_describe_meta("local", "Z", NA_character_, paste0 = TRUE, conn = conn)
-  expect_identical(meta, tibble::tibble(TableMeta = "LOCAL",
-                                        ColumnMeta = "Z",
-                                        MetaMeta = "class: logical",
-                                        DescriptionMeta = " More info. More info."))
-  meta <- rws_describe_meta("local", "Z", NA_character_, paste0 = FALSE, conn = conn)
+  meta <- rws_describe_meta("local", "Z", NA_character_, conn = conn)
   expect_identical(meta, tibble::tibble(TableMeta = "LOCAL",
                                         ColumnMeta = "Z",
                                         MetaMeta = "class: logical",
                                         DescriptionMeta = NA_character_))
+})
+
+test_that("describe two works", {
+  conn <- rws_connect(":memory:")
+  teardown(rws_disconnect(conn))
+  
+  local <- data.frame(z = c(TRUE, FALSE, NA), a = 1:3)
+  DBI::dbCreateTable(conn, "local", local)
+  expect_identical(rws_write(local, conn = conn), "local")
+  meta <- rws_describe_meta("local", "Z", "A logical vector.", conn = conn)
+  expect_identical(meta, tibble::tibble(TableMeta = c("LOCAL", "LOCAL"),
+                                        ColumnMeta = c("A", "Z"),
+                                        MetaMeta = c(NA, "class: logical"),
+                                        DescriptionMeta = c(NA, "A logical vector.")))
+  meta <- rws_describe_meta("local", "A", " More info.", conn = conn)
+  expect_identical(meta, tibble::tibble(TableMeta = c("LOCAL", "LOCAL"),
+                                        ColumnMeta = c("A", "Z"),
+                                        MetaMeta = c(NA, "class: logical"),
+                                        DescriptionMeta = c(" More info.", "A logical vector.")))
+
+  meta <- rws_describe_meta("local", "Z", "More info2.", conn = conn)
+  expect_identical(meta, tibble::tibble(TableMeta = c("LOCAL", "LOCAL"),
+                                        ColumnMeta = c("A", "Z"),
+                                        MetaMeta = c(NA, "class: logical"),
+                                        DescriptionMeta = c(" More info.", "More info2.")))
 })
