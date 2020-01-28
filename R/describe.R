422569f9d7212fc542128cbffd4dc9b0512de99c
joethorley <joe@poissonconsulting.ca>
2020-01-28 13:47:15 -0800

    chk cran version and update deprecated chk functions

--- R/describe.R
+++ R/describe.R
@@ -31,13 +31,13 @@ rws_describe_meta <- function(x, ..., conn) {
 #' rws_disconnect(conn)
 rws_describe_meta.character <- function(x, column, description, ..., conn) {
   chk_s3_class(x, "character")
-  chk_no_missing(x)
+  chk_not_any_na(x)
   chk_s3_class(column, "character")
-  chk_no_missing(column)
+  chk_not_any_na(column)
   chk_s3_class(description, "character")
   chk_sqlite_conn(conn, connected = TRUE)
   chk_unused(...)
-
+  
   rws_describe_meta(data.frame(
     Table = x, Column = column, Description = description,
     stringsAsFactors = FALSE
@@ -52,48 +52,47 @@ rws_describe_meta.character <- function(x, column, description, ..., conn) {
 #' @family rws_read
 #' @export
 rws_describe_meta.data.frame <- function(x, ..., conn) {
-  if (is_chk_on()) {
-    chk_s3_class(x, "data.frame")
-    chk_superset(colnames(x), c("Table", "Column", "Description"))
-    chk_s3_class(x$Table, "character")
-    chk_no_missing(x$Table)
-    chk_s3_class(x$Column, "character")
-    chk_no_missing(x$Column)
-    chk_s3_class(x$Description, "character")
-    chk_sqlite_conn(conn, connected = TRUE)
-    chk_unused(...)
-  }
+  chk_s3_class(x, "data.frame")
+  chk_superset(colnames(x), c("Table", "Column", "Description"))
+  chk_s3_class(x$Table, "character")
+  chk_not_any_na(x$Table)
+  chk_s3_class(x$Column, "character")
+  chk_not_any_na(x$Column)
+  chk_s3_class(x$Description, "character")
+  chk_sqlite_conn(conn, connected = TRUE)
+  chk_unused(...)
+  
   if (!nrow(x)) {
     return(invisible(rws_read_meta(conn)))
   }
-
+  
   x <- x[c("Table", "Column", "Description")]
   x$Table <- to_upper(x$Table)
   x$Column <- to_upper(x$Column)
-
+  
   if (anyDuplicated(x[c("Table", "Column")])) {
     err("Columns 'Table' and 'Column' in data 'x' must be unique.")
   }
-
+  
   x$Row <- 1:nrow(x)
-
+  
   meta <- rws_read_meta(conn)
   meta$RowMeta <- 1:nrow(meta)
-
+  
   meta <- merge(meta, x,
-    by.x = c("TableMeta", "ColumnMeta"),
-    by.y = c("Table", "Column"), all = TRUE
+                by.x = c("TableMeta", "ColumnMeta"),
+                by.y = c("Table", "Column"), all = TRUE
   )
-
+  
   if (any(is.na(meta$RowMeta))) {
     err("All description tables and columns must exist in the meta table.")
   }
-
+  
   meta$DescriptionMeta[!is.na(meta$Row)] <- meta$Description[!is.na(meta$Row)]
   meta <- meta[order(meta$RowMeta), ]
-
+  
   meta <- meta[c("TableMeta", "ColumnMeta", "MetaMeta", "DescriptionMeta")]
-
+  
   replace_meta_table(meta, conn = conn)
   invisible(as_tibble_sf(meta))
 }
