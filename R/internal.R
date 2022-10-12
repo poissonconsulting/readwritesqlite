335865768b3dbd3b0d34883867666fe9cd1ae201
Joe Thorley <joe@poissonconsulting.ca>
2022-10-12 13:28:43 -0700

    switch from as.character() to format() for POSIXct

--- R/internal.R
+++ R/internal.R
@@ -27,11 +27,10 @@ tz <- function(x) {
   tz
 }
 
-
 sys_date_time_utc <- function() {
   date_time <- Sys.time()
   attr(date_time, "tzone") <- "UTC"
-  as.character(date_time, format = "%Y-%m-%d %H:%M:%S")
+  format(date_time, format = "%Y-%m-%d %H:%M:%S")
 }
 
 user <- function() {
